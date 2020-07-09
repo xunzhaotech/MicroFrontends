716d112651423a3796de4f70709e9cc605a41bfe
luyb <luyb@xunzhaotech.com>
2020-07-10 00:26:48 +0800

    fix -修改基础设置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-05-03 21:39:48
- * @LastEditTime: 2020-07-09 14:18:07
+ * @LastEditTime: 2020-07-10 00:25:13
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \MicroFrontends\docs\.vuepress\config.js
@@ -11,9 +11,9 @@ const themeConfig = require('./themeConfig/')
 module.exports = {
   base: '/liangshuangshuang/',
   dest: '.vuepress/dist',
-  title: '微服务',
-  description: '一个有自由创作灵感的平台！',
-  theme: 'antdocs',
+  title: '写给小猪头LOVING',
+  description: '情不知所起却一往情深！',
+  theme: 'loving',
   host: '0.0.0.0',
   port: '80',
   themeConfig
