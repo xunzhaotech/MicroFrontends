dd701a2b2994b80d27574416671553582c684ee4
luyb <luyb@xunzhaotech.com>
2020-07-11 11:42:14 +0800

    fix -修改配置文档路径设置

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-06-23 16:38:32
- * @LastEditTime: 2020-07-10 16:24:53
+ * @LastEditTime: 2020-07-11 11:40:26
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \MicroFrontends\docs\.vuepress\themeConfig\index.js
@@ -9,15 +9,15 @@
 // 引入基本配置
 // const config = require('./config/')
 // 引入导航栏
-const nav = require('./nav')
+const nav = require('./themeConfig/nav')
 // 引入侧边栏
-const  sidebar = require('./sidebar')
+const  sidebar = require('./themeConfig/sidebar')
 // 默认为左侧底部设计
-const navs = require('./navs')
+const navs = require('./themeConfig/navs')
 // 
-const comment= require('./comment')
-const noteConfig= require('./noteConfig')
-const footer= require('./footer')
+const comment= require('./themeConfig/comment')
+const noteConfig= require('./themeConfig/noteConfig')
+const footer= require('./themeConfig/footer')
 module.exports = {
     logo: '/favicon.ico',
     author: "xunzhaotech",
@@ -25,7 +25,7 @@ module.exports = {
     docsDir: "docs",
     editLinks: false,
     editLinkText: "在 GitHub 上编辑此页",
-    lastUpdated: "最近更新时间",
+    lastUpdated: "最近更新",
     repo: 'xunzhaotech/MicroFrontends',
     nav,
     navs,
