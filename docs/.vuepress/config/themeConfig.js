191512a373b9ca36015164588bae7867bafc2f51
luyb <luyb@xunzhaotech.com>
2020-07-11 15:37:51 +0800

    fix -添加配置文件设置

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-06-23 16:38:32
- * @LastEditTime: 2020-07-11 11:40:26
+ * @LastEditTime: 2020-07-11 15:37:31
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \MicroFrontends\docs\.vuepress\themeConfig\index.js
@@ -19,14 +19,15 @@ const comment= require('./themeConfig/comment')
 const noteConfig= require('./themeConfig/noteConfig')
 const footer= require('./themeConfig/footer')
 module.exports = {
-    logo: '/favicon.ico',
-    author: "xunzhaotech",
-    searchMaxSuggestions: 20,
-    docsDir: "docs",
-    editLinks: false,
-    editLinkText: "在 GitHub 上编辑此页",
-    lastUpdated: "最近更新",
-    repo: 'xunzhaotech/MicroFrontends',
+    logo: '/favicon.ico',                // 导航栏logo
+    author: "xunzhaotech",               // 作者
+    searchMaxSuggestions: 20,            // 搜索结果显示最大数
+    sidebarDepth: 2,                     // 侧边栏显示深度，默认1，最大2（显示到h3标题）
+    docsDir: "docs",                     // 编辑的文件夹
+    editLinks: false,                    // 启用编辑
+    editLinkText: "编辑",                 // 修改编辑标签内容
+    lastUpdated: "最近更新",              // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
+    repo: 'xunzhaotech/MicroFrontends',  // 导航栏右侧生成Github链接
     nav,
     navs,
     sidebar,
